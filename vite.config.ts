0417a6a1aa35139999558febe4a56705331e5985
Pelayo Trives <pelayotrives@gmail.com>
2026-08-10 22:59:34 +0900

    feat: update language to English in index.html and adjust canonical link

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ui-portfolio-2026/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
